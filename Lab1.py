902e794fbbaef430aa18ad6a51fb156d0455f707
lina-alotri <lina.literati@gmail.com>
2017-02-24 17:01:46 -0800

    Create Lab1.py

---
+++ Lab1.py
@@ -0,0 +1,45 @@
+print ("My First Python Lab")
+
+print ("a      a^2    a^3")
+print ("1      1      1")
+print ("2      4      8")
+print ("3      9      27")
+print ("4      16     64")
+
+
+
+import turtle
+
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.forward(100)
+
+turtle.right(90)
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
